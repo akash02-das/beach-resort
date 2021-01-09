4360aa2f5ae930f02db2cf149cdf0b1bee61ac6f
Akash <aku.das02@gmail.com>
2021-01-09 22:04:14 +0600

    Page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.scss";
 
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello App</h1>
-      </div>
+      <>
+        <Home />
+        <Rooms />
+        <SingleRoom />
+        <Error />
+      </>
     );
   }
 }
