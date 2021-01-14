ff38bbeb54bbebedff18b3fee84e7d1ec8c154a7
Akash <aku.das02@gmail.com>
2021-01-14 19:44:02 +0600

    Size-Checkbox filter

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -95,6 +95,53 @@ const RoomsFilter = ({ rooms }) => {
           />
         </div>
         {/* End Room Price */}
+        {/* Size */}
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              className="size-input"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              className="size-input"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        {/* End Size */}
+        {/* Extras */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+        {/* End Extras */}
       </form>
     </section>
   );
--- src/context.js
+++ src/context.js
@@ -58,7 +58,7 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
 
     this.setState(
@@ -100,6 +100,21 @@ class RoomProvider extends Component {
 
     // Filter by Price
     tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    // Filter by Size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    // Filter by Breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    // Filter by Pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
 
     // Change State
     this.setState({ sortedRooms: tempRooms });
